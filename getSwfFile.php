df957c928bdb34a5a1766ad8f5da09e18b57f580
ProgramistaZpolski <piotrbadelek@protonmail.com>
2020-12-01 11:38:28 +0100

    Update getSwfFile.php

--- getSwfFile.php
+++ getSwfFile.php
@@ -1,4 +1,5 @@
 <?php
+// cors-anywhere does not support flash, so i had to resort to this thing.
 header('Content-Type: application/x-shockwave-flash');
 $url = $_GET['swfURL'];
 $resp = file_get_contents($url);
